74ab9969e71237974b1cedd6d00bfdc3b106e092
sasank-v19 <sasanksaiv19@gmail.com>
2026-02-25 16:42:24 +0530

    Final DevOps setup - Docker + CI/CD + Nginx

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
+app.use(cors());
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
